6df50b5ce352b79b11a40954f237918ff0fc5242
Alejandro Reyes <alejandroereyes@gmail.com>
2015-07-08 17:39:52 -0500

    created nav bar partial, added meth for class toggle to helpers, added root for home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def toggle_class_to(class_name, path_to_turn_on)
+    class_name if request.fullpath == path_to_turn_on
+  end
+
+  def active_if(path)
+    toggle_class_to('active', path)
+  end
 end
