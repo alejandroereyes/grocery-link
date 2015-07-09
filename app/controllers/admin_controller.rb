ee800fe9d9da4608563851f8704f512ae11c5a0c
Alejandro Reyes <alejandroereyes@gmail.com>
2015-07-08 22:11:31 -0500

    updated seed to pull from api to seed category table was successful, will update items to pull from api as well

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,6 @@ class AdminController < ApplicationController
   def dashboard
     @categories = im_lookup.lookup_get_categories
     # @product = im_link.products_search_products(name: "Amy's")
-    render json: @categories
+    @categories.inspect
   end
 end
