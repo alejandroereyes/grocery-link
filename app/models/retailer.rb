7006a7b1b602bc671bf73cdc05888625b534d479
Alejandro Reyes <alejandroereyes@gmail.com>
2015-07-09 18:40:48 -0500

    updated model with associations

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -1,2 +1,7 @@
 class Retailer < ActiveRecord::Base
+  has_many :stores
+  has_many :retailer_users
+  has_many :users, through: :retailer_users
+  has_many :retailer_item_prices
+  has_many :items, through: :retailer_item_prices
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :retailer_user
+  has_one :retailer, through: :retailer_user
 end
