500cfc9e59ef97757c719696b97b346e6c126b94
Alejandro Reyes <alejandroereyes@gmail.com>
2015-07-09 18:32:18 -0500

    added admin nav if admin logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :user_is_admin?
 
   def user_is_admin?
     @user_is_admin ||= current_user ? current_user.admin : false
