aa285ec7146f1ddd6b0f9d416a27387625fcd4fe
Alejandro Reyes <alejandroereyes@gmail.com>
2015-07-16 14:11:41 -0500

    working on docs page, added collapse fields but not working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,16 +10,19 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require react
 //= require react_ujs
 //= require components
+//= require jquery
 //= require materialize-sprockets
 //= require_tree .
 
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
+  $('.collapsible').collapsible({
+      accordion : false
+    });
 });
