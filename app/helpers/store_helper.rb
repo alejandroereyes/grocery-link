606ef2cf3198239760a327804a62ddee6f933d25
Alejandro Reyes <alejandroereyes@gmail.com>
2015-07-16 00:37:29 -0500

    updated body color to bone white

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,6 +1,7 @@
 module StoreHelper
   def us_states
-    states = %w(AL AK AZ AR CA CO CT DC DE FL GA HI ID IL IN IA KS KY LA ME MA MI MN MO MS
-                MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VA VT WA WI WV WY)
+    states = %w(AL AK AZ AR CA CO CT DC DE FL GA HI ID IL
+                  IN IA KS KY LA ME MA MI MN MO MS
+              MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VA VT WA WI WV WY)
   end
 end
