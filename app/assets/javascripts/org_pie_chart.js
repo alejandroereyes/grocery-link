540a2749af6c0b4129946641dca59470beb2a329
Alejandro Reyes <alejandroereyes@gmail.com>
2015-07-20 00:00:09 -0500

    fix issue accordian is working again

--- app/assets/javascripts/org_pie_chart.js
+++ app/assets/javascripts/org_pie_chart.js
@@ -1,34 +1,32 @@
-var dataPoints = [
+var dataPoints = [];
 
-                    ];
+function getOrgList() {
+  $.get('/admin/org_list', function (data) {
 
-  function getOrgList() {
-      $.get('/admin/org_list', function (data){
+    for (var i = 0; i < data.length; i++) {
+      dataPoints.push({ label: data[i]['id'], y: data[i]['org_count'] });
+    }; // for loop-end
+  }); // get-outer getOrgList-inner
+}; // getOrgList-outer
 
-        for (var i = 0; i < data.length; i++) {
-          dataPoints.push({ label: data[i]['id'], y: data[i]['org_count'] });
-        }; // for loop-end
-      }); // get-outer getOrgList-inner
-    }; // getOrgList-outer
+getOrgList();
 
-    getOrgList();
+window.onload = function () {
 
-  window.onload = function () {
+  var chart = new CanvasJS.Chart("chartContainer-for-me", {
 
-    var chart = new CanvasJS.Chart("chartContainer-for-me", {
+    title:{
+      text: "All Retailers Organic Count"
+    },
+    data: [//array of dataSeries
+      { //dataSeries object
 
-      title:{
-        text: "All Retailers Organic Count"
-      },
-      data: [//array of dataSeries
-        { //dataSeries object
+       /*** Change type "column" to "bar", "area", "line" or "pie"***/
+       type: "pie",
+       dataPoints : dataPoints
+      }
+     ]
+   });
 
-         /*** Change type "column" to "bar", "area", "line" or "pie"***/
-         type: "pie",
-         dataPoints : dataPoints
-        }
-       ]
-     });
-
-    chart.render();
-  }
+  chart.render();
+}
