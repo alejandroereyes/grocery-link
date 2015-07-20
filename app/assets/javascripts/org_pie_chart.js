11a9889c27d931532df5c9ba3e98b3b37a6d48ad
Alejandro Reyes <alejandroereyes@gmail.com>
2015-07-20 08:49:43 -0500

    fxd bug with chart, added .append to $get callback

--- app/assets/javascripts/org_pie_chart.js
+++ app/assets/javascripts/org_pie_chart.js
@@ -1,68 +1,68 @@
 // <<<<<<< HEAD
+// var dataPoints = [{label: "Welcome", y: 100}];
+
+// function getOrgList() {
+//   $.get('/admin/org_list', function (data) {
+
+//     for (var i = 0; i < data.length; i++) {
+//       dataPoints.push({ label: data[i]['id'], y: data[i]['org_count'] });
+//     }; // for loop-end
+//   }); // get-outer getOrgList-inner
+// }; // getOrgList-outer
+
+// // getOrgList();
+
+// var chart = new CanvasJS.Chart("chartContainer-for-me", {
+
+//   title:{
+//     text: "All Retailers Organic Count"
+//   },
+//   data: [{ type: "pie", dataPoints: dataPoints }] /*** Change type "column" to "bar", "area", "line" or "pie"***/
+//  });
+
+// window.onload = function () {
+//   dataPoints.shift();
+
+//   getOrgList();
+
+//   chart.render();
+// };
+// =======
+
+// setTimeout(function(){
+
+// chart = [create chart here]
+// chart.render();
+
+// },3000);
+
+var chart = null;
+
 var dataPoints = [];
 
 function getOrgList() {
   $.get('/admin/org_list', function (data) {
 
     for (var i = 0; i < data.length; i++) {
-      dataPoints.push({ label: data[i]['id'], y: data[i]['org_count'] });
-    }; // for loop-end
+      $("body")
+      .append(dataPoints.push({ label: data[i]['id'], y: data[i]['org_count'] }))
+      .append(chart.render());
+    };
+    // for loop-end
   }); // get-outer getOrgList-inner
 }; // getOrgList-outer
 
 getOrgList();
 
-window.onload = function () {
+chart = new CanvasJS.Chart("chartContainer-for-me", {
 
-  var chart = new CanvasJS.Chart("chartContainer-for-me", {
+  title:{
+    text: "All Retailers Organic Count"
+  },
+  data: [{ type: "pie", dataPoints: dataPoints }] /*** Change type "column" to "bar", "area", "line" or "pie"***/
+});
 
-    title:{
-      text: "All Retailers Organic Count"
-    },
-    data: [//array of dataSeries
-      { //dataSeries object
+chart.render();
 
-       /*** Change type "column" to "bar", "area", "line" or "pie"***/
-       type: "pie",
-       dataPoints : dataPoints
-      }
-     ]
-   });
 
-  chart.render();
-}
-// =======
-
-// $(document).ready( function getMyCoolChart() {
-
-//   var dataPoints = [];
-
-//   function getOrgList() {
-//     $.get('/admin/org_list', function (data) {
-
-//       for (var i = 0; i < data.length; i++) {
-//         dataPoints.push({ label: data[i]['id'], y: data[i]['org_count'] });
-//       }; // for loop-end
-//     }); // get-outer getOrgList-inner
-//   }; // getOrgList-outer
-
-//   getOrgList();
-
-//   var chart = new CanvasJS.Chart("chartContainer-for-me", {
-
-//     title:{
-//       text: "All Retailers Organic Count"
-//     },
-//     data: [//array of dataSeries
-//       { //dataSeries object
-
-//        /*** Change type "column" to "bar", "area", "line" or "pie"***/
-//        type: "pie",
-//        dataPoints : dataPoints
-//       }
-//     ]
-//   });
-
-//   chart.render();
-// });
 // >>>>>>> c6464dba1ceb64db3e764efb9c277f9f8027b35a
