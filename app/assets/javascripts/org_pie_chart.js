469330733d58fbe96c9a31d8c1649a3c23660290
Alejandro Reyes <alejandroereyes@gmail.com>
2015-07-20 14:20:44 -0500

    bug with datatable fixd, missing </tr> and fxd issue wth canvas js showing data rmvd .appnd and jush pshd array

--- app/assets/javascripts/org_pie_chart.js
+++ app/assets/javascripts/org_pie_chart.js
@@ -1,68 +1,30 @@
-// <<<<<<< HEAD
-// var dataPoints = [{label: "Welcome", y: 100}];
 
-// function getOrgList() {
-//   $.get('/admin/org_list', function (data) {
+$( document ).ready(function(){
+  if($('#chartContainer-for-me').length) {
+    var chart = null;
 
-//     for (var i = 0; i < data.length; i++) {
-//       dataPoints.push({ label: data[i]['id'], y: data[i]['org_count'] });
-//     }; // for loop-end
-//   }); // get-outer getOrgList-inner
-// }; // getOrgList-outer
+    var dataPoints = [];
 
-// // getOrgList();
+    function getOrgList() {
+      $.get('/admin/org_list', function (data) {
 
-// var chart = new CanvasJS.Chart("chartContainer-for-me", {
+        for (var i = 0; i < data.length; i++) {
+          dataPoints.push({ label: data[i]['id'], y: data[i]['org_count'] })
+        } // for loop-end
+        chart.render();
+      }); // get-outer getOrgList-inner
+    }; // getOrgList-outer
 
-//   title:{
-//     text: "All Retailers Organic Count"
-//   },
-//   data: [{ type: "pie", dataPoints: dataPoints }] /*** Change type "column" to "bar", "area", "line" or "pie"***/
-//  });
+    getOrgList();
 
-// window.onload = function () {
-//   dataPoints.shift();
 
-//   getOrgList();
+    chart = new CanvasJS.Chart("chartContainer-for-me", {
 
-//   chart.render();
-// };
-// =======
-
-// setTimeout(function(){
-
-// chart = [create chart here]
-// chart.render();
-
-// },3000);
-
-var chart = null;
-
-var dataPoints = [];
-
-function getOrgList() {
-  $.get('/admin/org_list', function (data) {
-
-    for (var i = 0; i < data.length; i++) {
-      $("body")
-      .append(dataPoints.push({ label: data[i]['id'], y: data[i]['org_count'] }))
-      .append(chart.render());
-    };
-    // for loop-end
-  }); // get-outer getOrgList-inner
-}; // getOrgList-outer
-
-getOrgList();
-
-chart = new CanvasJS.Chart("chartContainer-for-me", {
-
-  title:{
-    text: "All Retailers Organic Count"
-  },
-  data: [{ type: "pie", dataPoints: dataPoints }] /*** Change type "column" to "bar", "area", "line" or "pie"***/
+      title:{
+        text: "All Retailers Organic Count"
+      },
+      data: [{ type: "pie", dataPoints: dataPoints }] /*** Change type "column" to "bar", "area", "line" or "pie"***/
+    });
+  }
 });
 
-// chart.render();
-
-
-// >>>>>>> c6464dba1ceb64db3e764efb9c277f9f8027b35a
