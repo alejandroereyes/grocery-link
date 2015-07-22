ba5b99605fd3c060dcc3620c4641b446cd496028
Alejandro Reyes <alejandroereyes@gmail.com>
2015-07-22 17:37:20 -0500

    updated a test

--- test/models/store_test.rb
+++ test/models/store_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class StoreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @retailer = Retailer.create(name: "HEB")
+    @store = Store.create(store_id: Faker::Number.number(4), retailer_id: @retailer.id,
+                          street: Faker::Address.street_name, city: Faker::Address.city,
+                          state: Faker::Address.state, zip: Faker::Number.number(5))
+  end
+
+  test "store exists" do
+    assert @store
+  end
 end
